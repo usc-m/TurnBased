c45f0d1de2a9fe63fcbdfbc34a624e57f3decaf3
fpapado <fgpapado@gmail.com>
2013-03-13 22:46:53 +0000

    Added Map functionality, fixed merge issues

--- TurnBased/src/inf1/oop/turnbased/screen/MapScreen.java
+++ TurnBased/src/inf1/oop/turnbased/screen/MapScreen.java
@@ -1,16 +1,17 @@
 package inf1.oop.turnbased.screen;
 
 import inf1.oop.turnbased.ServiceProvider;
-//import inf1.oop.turnbased.entity.Entity;
 import inf1.oop.turnbased.graphics.MapRenderer;
 import inf1.oop.turnbased.map.Map;
 import inf1.oop.turnbased.map.Tile;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MapScreen extends Screen {
@@ -19,6 +20,8 @@ public class MapScreen extends Screen {
 	SpriteBatch batch;
 	MapRenderer renderer;
 	Camera camera;
+	AssetManager assets;
+	
 	
 	Map map;
 
@@ -29,25 +32,23 @@ public class MapScreen extends Screen {
 	
 	
 	//VARIABLES, load these from a .json file later on?
-	int map_height=20; //map slots
-	int map_width=20; //map slots
-	int grid_size=16; //pixels
-	int map_pixelheight=map_height*grid_size; //map total height in pixels
-	int map_pixelwidth=map_width*grid_size; //map total width in pixels
-	int map_centerx=Gdx.graphics.getWidth()/2; //center of screen on x-axis
-	int map_centery=Gdx.graphics.getHeight()/2; //center of screen on y-axis
+	int margin_bottom=0;//distance starting to draw grid, number is index?
+	int margin_left=0; 	//same from left, number is index?
+	int map_height=20;	//map height in tiles
+	int map_width=20; 	//map width in tiles
+	int grid_size=16; 	//tile size in px
+	int map_pixelheight=map_height*grid_size; 	 //map total height in pixels
+	int map_pixelwidth=map_width*grid_size; 	 //map total width in pixels
+	int map_centerx=Gdx.graphics.getWidth()/2;	 //center of screen on x-axis
+	int map_centery=Gdx.graphics.getHeight()/2;	 //center of screen on y-axis
 	
-	//VARIABLES
-	int player_x=0; //x-position of player (number is the grid index, so NOT in pixels)
-	int player_y=0; //y-position of player (number is the grid index, so NOT in pixels)
-	
-
-	//Entity obj_Player = new Entity(5,5,"player",10,4,100);
-	//obj_Player.setY(2);
-
-	
+	//Player Data
+	int player_x=0; //player x-position in px
+	int player_y=0; //yplayer y-position in px
+	Texture playerS;
 	
 	public MapScreen(ServiceProvider services) {
+		assets = services.get(AssetManager.class);
 		this.services = services;
 		
 		float w = Gdx.graphics.getWidth();
@@ -59,18 +60,19 @@ public class MapScreen extends Screen {
 		batch = services.get(SpriteBatch.class);
 		
 		//DRAW GRID :: logic error on margin_left and margin_bottom, try to puzzle it out
-		map = new Map(map_height,map_width, grid_size, grid_size); //<-- what do the 5's mean?
+		map = new Map(map_height+margin_bottom,map_width+margin_left, grid_size, grid_size);
 		for (int width=0; width<map_width; width+=1)
 		{
 			for (int height=0; height<map_height; height+=1)
 			{
-				map.setTile(width, height, new Tile("assets/data/spr_EmptySquare.png"));
+				//will be subbed with generate map method, suggest removing(i.e. integrating in tile class) the pathing
+				map.setTile(margin_bottom+width, margin_left+height, new Tile("assets/data/spr_EmptySquare.png"));
 			}
 		}
 		//------------------------------------------------------
 		
 		renderer.setMap(map);
-		
+		playerS = assets.get("assets/data/spr_Player.png");
 	}
 	
 	@Override
@@ -80,8 +82,9 @@ public class MapScreen extends Screen {
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		renderer.draw(map_centerx-(map_pixelwidth/2),0); //map draw margins, precisely centered on x-axis, 0 margin on y
-		batch.end();
+		renderer.draw(map_centerx-(map_pixelwidth/2),0); //map draw margins, centered on x-axis, 0 margin on y
+	    batch.draw(playerS, player_x, player_y);
+	    batch.end();
 		
 		
 		
@@ -94,70 +97,55 @@ public class MapScreen extends Screen {
 		 * DRAW PLAYER, put this as method in external class later * <-- and then run in the render method instead of create (create only runs ONCE, at game start)
 		 * ####################################################### */
 		
-		//PLAYER MOVEMENT USING ARROW KEYS
-		//ERROR: code does not update/trigger on key press, tested this with the System.out.println.
+		//PLAYER MOVEMENT USING ARROW KEYS	
 		
-		player_walktimer-=1;
-		
-		if (player_walktimer <= 0)
+		//moving left
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) 
 		{
-			//moving left
-			if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) 
+			if (player_x > 0 - map.getWidth()*map.getTileHeight()/2 ) //sub 16 with tilesize passed form class above -F
 			{
-				map.setTile(player_x, player_y, new Tile("assets/data/spr_EmptySquare.png"));
-				if (player_x > 0)
-				{
-					player_x -= 1; 
-					System.out.println("player_x: "+player_x);
-					player_walktimer=player_walktimertotal;
-				}
-			} //take away the println parts when working.
-			
-			//moving right
-			else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) 
-			{
-				map.setTile(player_x, player_y, new Tile("assets/data/spr_EmptySquare.png"));
-				if (player_x+1 < map_width)
-				{
-					player_x += 1; 
-					System.out.println("player_x: "+player_x);
-					player_walktimer=player_walktimertotal;
-				}
+				player_x -= 1; 	//consider using delta and f values -F
+				System.out.println("player_x: "+player_x);
+				player_walktimer=player_walktimertotal;
 			}
+		} //take away the println parts when working.
 			
-			//moving up
-			else if (Gdx.input.isKeyPressed(Input.Keys.UP)) 
+		//moving right
+		else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) 
+		{
+			if (player_x +16 < 0 + map.getWidth()*map.getTileHeight()/2) //there is a +16 adjustment to playerX since detection point is bottom left
 			{
-				map.setTile(player_x, player_y, new Tile("assets/data/spr_EmptySquare.png"));
-				if (player_y+1 < map_height)
-				{
-					player_y += 1; 
-					System.out.println("player_y: "+player_y);
-					player_walktimer=player_walktimertotal;
-				}
-			}
-			
-			//moving down
-			else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) 
-			{
-				map.setTile(player_x, player_y, new Tile("assets/data/spr_EmptySquare.png"));
-				if (player_y > 0)
-				{
-					player_y -= 1; 
-					System.out.println("player_y: "+player_y);
-					player_walktimer=player_walktimertotal;
-				}
+				player_x += 1; 
+				System.out.println("player_x: "+player_x);
+				player_walktimer=player_walktimertotal;
 			}
 		}
-		
-		// :: DRAW PLAYER ::
+			
+		//moving up
+		else if (Gdx.input.isKeyPressed(Input.Keys.UP)) 
+		{
+			if (player_y + 16 < 0 + map.getHeight()*map.getTileHeight()/2)
+			{
+				player_y += 1; 
+				System.out.println("player_y: "+player_y);
+				player_walktimer=player_walktimertotal;
+			}
+		}
+			
+		//moving down
+		else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) 
+		{
+			if (player_y > 0 - map.getHeight()*map.getTileHeight()/2)
+			{
+				player_y -= 1; 
+				System.out.println("player_y: "+player_y);
+				player_walktimer=player_walktimertotal;
+			}
+		}
+	}
 		//IMPORTANT -> margin_bottom and margin_left must come from the map-drawing code above.
 		//do not make separate variables with the same definition, important when putting these blocks in external classes.
 		//ie. load from the same .json file as from the "DRAW MAP" block above
-		map.setTile(player_x, player_y, new Tile("assets/data/spr_Player.png"));
 		//------------------------------------------------------
-		
-		
-	}
 
 }
